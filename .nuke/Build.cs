cf4586384886672a0f31fc3c143a80af85d94dbe
SKProCH <skproch@yandex.ru>
2024-06-22 02:15:22 +0300

    Revert "Tries to hide the log output"
    This reverts commit 4ce311fcb0d7d926969e6b1d07c9d86b3460b7cd.

--- .nuke/Build.cs
+++ .nuke/Build.cs
@@ -118,7 +118,7 @@ class Build : NukeBuild
                 BuildCommand +
                 $" /p:Version={version.DoubleQuoteIfNeeded().ReplaceCommas()}" +
                 $" /p:PackageReleaseNotes={releaseNotes.DoubleQuoteIfNeeded().ReplaceCommas()}", 
-                logger: DotNetTasks.DotNetLogger, logOutput: false);
+                logger: DotNetTasks.DotNetLogger);
             buildProcess.AssertZeroExitCode();
         });
 
