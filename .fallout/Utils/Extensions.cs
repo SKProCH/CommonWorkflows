80793d6fe44b74912a169bc4a0e41fcea3795029
SKProCH <skproch@yandex.ru>
2026-08-11 01:11:25 +0300

    Improve nightly version check

--- .fallout/Utils/Extensions.cs
+++ .fallout/Utils/Extensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Immutable;
+﻿using System;
 using System.Linq;
 using NuGet.Versioning;
 
@@ -7,14 +7,8 @@ namespace Utils;
 public static class Extensions
 {
     public static bool IsNightly(this NuGetVersion version) {
-        if (version.ToString().Contains("nightly")) {
-            return true;
-        }
-
-        // 3.2.5-nightly.0.1
-        // If x.y on the end - this is nightly
-        var lastLabels = version.ReleaseLabels.TakeLast(2).ToImmutableArray();
-        return lastLabels.Length == 2 && lastLabels.All(s => int.TryParse(s, out _));
+        return version.ReleaseLabels.FirstOrDefault()
+            ?.Equals("nightly", StringComparison.OrdinalIgnoreCase) == true;
     }
 
     public static string ReplaceMsBuildCharacters(this string s) =>
